3fe210ec51fa4cf0ca9b42f316b5790d0270b30d
SergioMartinezGarcia <sergio.m@opendeusto.es>
2025-12-04 09:41:51 +0100

    Uploaded the new files for spring boot 3.5.8

---
+++ Group3/src/test/java/com/unipd/dei/sp/Group3/TestcontainersConfiguration.java
@@ -0,0 +1,8 @@
+package com.unipd.dei.sp.Group3;
+
+import org.springframework.boot.test.context.TestConfiguration;
+
+@TestConfiguration(proxyBeanMethods = false)
+class TestcontainersConfiguration {
+
+}
