36e12fc9d78cbd20d5c02a5dae823002b72ca372
SergioMartinezGarcia <sergio.m@opendeusto.es>
2025-12-04 09:03:56 +0100

    Delete the files of the project to use another spring boot version

--- Group3/src/test/java/com/unipd/dei/sp/Group3/TestcontainersConfiguration.java
+++
@@ -1,8 +0,0 @@
-package com.unipd.dei.sp.Group3;
-
-import org.springframework.boot.test.context.TestConfiguration;
-
-@TestConfiguration(proxyBeanMethods = false)
-class TestcontainersConfiguration {
-
-}
